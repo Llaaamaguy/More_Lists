ff0462a2c393ed1e6dc80192cb6f02e3ff10b05c
Llaaamaguy <67341568+Llaaamaguy@users.noreply.github.com>
2020-11-12 14:51:50 -0500

    Update lists.py

--- lists.py
+++ lists.py
@@ -1,17 +1,33 @@
 def sum_of_odd_nums(n):
-    pass
+    n = n * 2
+    numbers = [i for i in range(1,n) if i%2 == 1]
+    return sum(numbers)
 
 def caesar_cipher(message, key):
-    pass
+    ciphernumbers = [ord(i)+key for i in message]
+    decoded = bytes(ciphernumbers).decode("ascii")
+    return decoded
+
+def fbhelper(n):
+    if n%3 == 0 and n%5 == 0:
+        return "fizzbuzz"
+    elif n%3 == 0:
+        return "fizz"
+    elif n%5 == 0:
+        return "buzz"
+    else:
+        return n
+
 
 def fizzbuzz(n):
-    pass 
+    numlist = [fbhelper(i) for i in range(1,n)]
+    return numlist
 
 def main():
     print('Table of the sum for the first n odd numbers:')
     print('n\tsum')
     print('-'*16)
-    for n in range(10):
+    for n in range(1,11):
         print('{}\t{}'.format(n, sum_of_odd_nums(n)))
 
     print()
